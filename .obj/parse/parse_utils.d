2360e8d8263921002fa7c0a898ff7ca54174ede6
Paul Arden <parden@f4r11s8.clusters.42paris.fr>
2025-07-06 17:57:54 +0200

    Finish parser design

--- .obj/parse/parse_utils.d
+++
@@ -1,4 +0,0 @@
-.obj/parse/parse_utils.o: src/parse/parse_utils.cpp \
-  src/parse/../../inc/parse/parse_utils.hpp
-
-src/parse/../../inc/parse/parse_utils.hpp:
--- .obj/parse/startLine.d
+++
@@ -1,7 +0,0 @@
-.obj/parse/startLine.o: src/parse/startLine.cpp \
-  src/parse/../../inc/parse/parse.hpp \
-  src/parse/../../inc/parse/parse_utils.hpp
-
-src/parse/../../inc/parse/parse.hpp:
-
-src/parse/../../inc/parse/parse_utils.hpp:
