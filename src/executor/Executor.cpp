1ce263d001a9d1e1def80d12b8fd0be4ce2784ea
racoutte <racoutte@student.42.fr>
2025-07-21 15:59:41 +0200

    commencement updateResponseData avec content-type et page erreur perso, a creuser pour le reste

--- src/executor/Executor.cpp
+++ src/executor/Executor.cpp
@@ -1,3 +1,5 @@
+#include <fstream>
+#include <sstream>
 #include "Executor.hpp"
 #include "GetExecutor.hpp"
 #include "PostExecutor.hpp"
@@ -8,18 +10,40 @@
 
 void	Executor::updateResponseData(Conversation& conv)
 {
-	//content-type encore a creuser -- updates en fonction des erreurs...
-	if (conv.resp.contentType.empty())
-		conv.resp.contentType = getMimeType(conv.req.uri); //a revoir d'ou je recupere l'extension..
-		//ex: 404.html est pathOnDisk: est-ce stocke avant dans conv.req.uri ou pas? donc pas forcement conv.req.uri..
+	if (conv.resp.status >= 400)
+	{
+		std::map<int, std::string>& errorPages = conv.config.errorPagesCodes;
+		std::map<int, std::string>::iterator it = errorPages.find(conv.resp.status);
+
+		if (it != errorPages.end())
+		{
+			const std::string& errorPagePath = it->second;
+
+			std::ifstream file;
+			file.open(errorPagePath.c_str());
+
+			if (file.is_open())
+			{
+				std::ostringstream ss;
+				ss << file.rdbuf();
+				conv.resp.body = ss.str();
+				conv.resp.contentType = getMimeType(errorPagePath);
+			}
+			else
+			{
+				conv.resp.body = "<html><body><h1>" + intToString(conv.resp.status) + " Error</h1></body></html>";
+				conv.resp.contentType = "text/html";
+			}
+		}
+	}
 
 	if ((conv.resp.status == CREATED || (conv.resp.status >= 300 && conv.resp.status < 400))
 		&& conv.resp.location.empty())
 	{
-		conv.resp.location = conv.req.uri;
+		//MAJ avec la location nouvellement creee
 	}
 
-	// gerer cookies aussi ?
+	//aussi pour le cas 200 + empty (100 n'a pas de content-type)
 }
 
 
@@ -73,7 +97,11 @@ void	Executor::execute(Conversation& conv)
 void	Executor::executeStart(Conversation& conv)
 {
 	if (conv.resp.status != NOT_A_STATUS_CODE && conv.resp.status != CONTINUE)
+	{
+		updateResponseData(conv);
+		//check error + MAJ struct response (content-type + body html des erreurs)
 		return;
+	}
 
 	const std::string& method = conv.req.method;
 
