fcc27f88d3559e52936185630fb71d37774b0f84
Thomas Chobert <tchobert@f4r3s1.clusters.42paris.fr>
2025-08-04 19:31:31 +0200

    Zinedine Zidane le parser

--- test/config_parse/test_config_parser.cpp
+++ test/config_parse/test_config_parser.cpp
@@ -1,11 +1,21 @@
 #include "ConfigParser.hpp"
 
+void printConfig(const serverConfig& config) {
+
+	std::cout << "Has root: " << config.identity.hasRoot << std::endl;
+	std::cout << "root: " << config.identity.root << std::endl;
+	std::cout << "host: " << config.identity.host << std::endl;
+	std::cout << "port: " << config.identity.port << std::endl;
+	std::cout << "name: " << config.identity.serverName << std::endl;
+}
+
 int main(int ac, char **av) {
 
 	if (ac != 2)
 		return (EXIT_FAILURE);
 
 	std::string config = av[1];
+	std::vector<serverConfig> configs;
 
 	ConfigFileReader reader;
 	HeaderSectionParser headerParser;
@@ -14,5 +24,9 @@ int main(int ac, char **av) {
 	ConfigParser parser(config, reader, headerParser, serverParser, locParser);
 
 	parser.parse();
+	configs = parser.getConfigs();
+	for (std::vector<serverConfig>::const_iterator it = configs.begin(); it != configs.end(); ++it) {
+		printConfig(*it);
+	}
 	return (EXIT_SUCCESS);
 }
